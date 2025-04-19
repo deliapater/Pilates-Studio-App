508083a5c08f1e78eb3ea20a1ed98fc348b275d0
Delia Paternina <delia.paternina@gmail.com>
2025-04-19 12:22:46 -0400

    adds jest testing setup

---
+++ jest.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+    transform: {
+      '^.+\\.vue$': 'vue-jest',
+      '^.+\\.js$': 'babel-jest',
+    },
+    moduleFileExtensions: ['js', 'json', 'vue'],
+    testEnvironment: 'jsdom',
+  };
